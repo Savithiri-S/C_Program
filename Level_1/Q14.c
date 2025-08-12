d4db2f110a987f7936e8be648e4ca212f09283c9
Savithiri <savithirikg2024@gmail.com>
2025-08-12 10:53:49 +0530

    Q13-15

---
+++ Level_1/Q14.c
@@ -0,0 +1,8 @@
+#include<stdio.h>
+int main()
+{
+    int a=20,b=30,c=40,x;
+    x = a+b+c;
+    printf("Sum : %d",x);
+    return 0;
+}
---
+++ Level_1/Q15.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+#include<conio.h>
+int main()
+{
+    int a,b,c,x;
+    scanf("%d",&a);
+    scanf("%d",&b);
+    scanf("%d",&c);
+    x = a+b+c;
+    printf("Sum : %d",x);
+    return 0;
+}
